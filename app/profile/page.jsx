1b024c79d82f4000b220fe7bf6534f4675b7d392
Avranil Maity <avranilmaity97@gmail.com>
2023-11-04 16:16:17 +0530

    feat: promptopia app base

---
+++ app/profile/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+const MyProfile = () => {
+  const [posts, setPosts] = useState([]);
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const userResponse = confirm("Are you sure?");
+    if (userResponse) {
+      try {
+        await fetch(`api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`, {
+        method: "GET",
+      });
+
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
